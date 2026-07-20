458f2e62628b0d880f238c8bd60c626805e89e15
mxwiser <neo@qvqol.com>
2026-07-20 10:46:57 +0800

    add test file

--- udp.py
+++ udp.py
@@ -1,18 +1,20 @@
 import socket
+import time
+fpga_ip = "192.168.15.14"
+fpga_port = 8080
 
-FPGA = ("192.168.15.14", 6000)   # 目标IP、目标端口(随意)
-SRC_PORT = 6000                  # 本机发送用的源端口
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(("192.168.15.13", 8081))
+sock.settimeout(2)
 
-tx = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-tx.bind(("0.0.0.0", SRC_PORT))
-tx.sendto(b"hello fpga", FPGA)
+while True:
+    data = b"123456"
 
-# 你的设计回包发到"源端口+1"，所以监听 5001
-rx = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-rx.bind(("0.0.0.0", SRC_PORT + 1))
-rx.settimeout(2)
-try:
-    data, addr = rx.recvfrom(2048)
-    print("收到回包:", data, "来自:", addr)
-except socket.timeout:
-    print("超时，无回包")
+    sock.sendto(data, (fpga_ip, fpga_port))
+    print("send:", data)
+    time.sleep(0.01)
+    try:
+        rx, addr = sock.recvfrom()
+        print("recv:", rx, "from", addr)
+    except:
+        print("timeout")
