8e7166278c3602c8a92825ef0cb9ab350e185aa7
mxwiser <neo@qvqol.com>
2026-07-20 11:30:42 +0800

    update

--- udp.py
+++ udp.py
@@ -1,20 +1,19 @@
 import socket
 import time
 fpga_ip = "192.168.15.14"
-fpga_port = 8080
+fpga_port = 8090
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(("192.168.15.13", 8081))
 sock.settimeout(2)
-
+sock.setblocking(False)
 while True:
-    data = b"123456"
-
+    data = b"Hello world! Make Altera Great Again!"
     sock.sendto(data, (fpga_ip, fpga_port))
     print("send:", data)
-    time.sleep(0.01)
     try:
-        rx, addr = sock.recvfrom()
+        rx, addr = sock.recvfrom(1024)
         print("recv:", rx, "from", addr)
     except:
         print("timeout")
+    time.sleep(0.1)
